f494f08ea4b1f7f3800f29db0f7224d2571a4576
Int <intsdev@gmail.com>
2018-11-26 16:50:39 +0200

    add animation for router

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,14 @@
   <div>
 
     <app-header></app-header>
-    <router-view></router-view>
+
+    <transition name="fade"
+                enter-active-class="fade-in"
+                leave-active-class="hidden"
+                mode="out-in">
+
+      <router-view></router-view>
+    </transition>
 
   </div>
 </template>
@@ -34,4 +41,39 @@
     margin: 25px auto;
     text-align: center;
   }
+
+
+  /* Animations Styles */
+  .hidden {
+    display: none;
+  }
+  .fade-in {
+    -webkit-animation: fadein 1s;
+    -moz-animation: fadein 1s;
+    -ms-animation: fadein 1s;
+    -o-animation: fadein 1s;
+    animation: fadein 1s;
+  }
+
+  @keyframes fadein {
+    from { opacity: 0; }
+    to   { opacity: 1; }
+  }
+
+  @-moz-keyframes fadein {
+    from { opacity: 0; }
+    to   { opacity: 1; }
+  }
+
+  @-webkit-keyframes fadein {
+    from { opacity: 0; }
+    to   { opacity: 1; }
+  }
+
+  @-ms-keyframes fadein {
+    from { opacity: 0; }
+    to   { opacity: 1; }
+  }
+
+
 </style>
