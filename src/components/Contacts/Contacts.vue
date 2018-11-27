741292b332da0032a933f78a84911909e3657625
Int <intsdev@gmail.com>
2018-11-28 00:14:40 +0200

    add highlight.js

--- src/components/Contacts/Contacts.vue
+++ src/components/Contacts/Contacts.vue
@@ -1,43 +1,45 @@
 <template>
-  <div class="container">
-    <section class="block">
-      <h1>Contact</h1>
-      <hr>
-        <p>Вы работаете над чем-то великим? Я хочу помочь вам в этом! Напишите мне и мы начнем работу над вашим проектом! Just do it!</p>
-      <hr>
+  <div>
+    <div class="container">
+      <section class="block">
+        <h1>Contact</h1>
+        <hr>
+          <p>Вы работаете над чем-то великим? Я хочу помочь вам в этом! Напишите мне и мы начнем работу над вашим проектом! Just do it!</p>
+        <hr>
 
-      <transition name="flip"
-                  enter-active-class="animated fadeIn"
-                  leave-active-class="animated fadeOut"
-                  mode="out-in">
+        <transition name="flip"
+                    enter-active-class="animated fadeIn"
+                    leave-active-class="animated fadeOut"
+                    mode="out-in">
 
-        <form v-if="!isDone" action="" @submit.prevent="sendMessage">
-          <fieldset :disabled="isSending">
-            <appField v-for="(item, i) in info"
-                  :name="item.name"
-                  :value="item.value"
-                  :pattern="item.pattern"
-                  :key="i"
-                  :type="item.type"
-                  @update:input="onUpdateInput(i, $event)"
-                  @update:valid="onValidInput(i, $event)">
-            </appField>
+          <form v-if="!isDone" action="" @submit.prevent="sendMessage">
+            <fieldset :disabled="isSending">
+              <appField v-for="(item, i) in info"
+                    :name="item.name"
+                    :value="item.value"
+                    :pattern="item.pattern"
+                    :key="i"
+                    :type="item.type"
+                    @update:input="onUpdateInput(i, $event)"
+                    @update:valid="onValidInput(i, $event)">
+              </appField>
 
-            <button class="btn btn-primary" :disabled="!formReady">
-              <span v-if="isSending"><icon  name="spinner" class="fa-pulse"/> One moment please</span>
-              <span v-else>Отправить</span>
-            </button>
-          </fieldset>
-        </form>
-        <div v-else>
-          <b-alert show dismissible variant="success" @dismissed="onAlertClose">
-            <h4 class="text-center">Спасибо за Ваше сообщение! Я свяжусь с вами как можно скорее!</h4>
-          </b-alert>
-        </div>
+              <button class="btn btn-primary" :disabled="!formReady">
+                <span v-if="isSending"><icon  name="spinner" class="fa-pulse"/> One moment please</span>
+                <span v-else>Отправить</span>
+              </button>
+            </fieldset>
+          </form>
+          <div v-else>
+            <b-alert show dismissible variant="success" @dismissed="onAlertClose">
+              <h4 class="text-center">Спасибо за Ваше сообщение! Я свяжусь с вами как можно скорее!</h4>
+            </b-alert>
+          </div>
 
-      </transition>
+        </transition>
 
-    </section>
+      </section>
+    </div>
   </div>
 </template>
 
--- src/components/Project/Project.vue
+++ src/components/Project/Project.vue
@@ -2,9 +2,7 @@
   <div>
     <div class="container" v-if="getProject(name)">
       <section class="block">
-
         <div v-html="projectData"></div>
-
       </section>
     </div>
     <e404 v-else></e404>
@@ -13,8 +11,16 @@
 
 <script>
   import E404 from './../E404';
-
   import { mapGetters, mapActions } from 'vuex';
+
+  import hljs from 'highlight.js/lib/highlight';
+  import javascript from 'highlight.js/lib/languages/javascript';
+  hljs.registerLanguage('javascript', javascript);
+
+  // import 'highlight.js/styles/dracula.css'
+  // import 'highlight.js/styles/solarized-dark.css'
+  import 'highlight.js/styles/solarized-light.css'
+  // import 'highlight.js/styles/monokai-sublime.css'
 
   export default {
     components: { E404 },
